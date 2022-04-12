0048c036696222ab8c004bc76ad2a21af6bdc0b5
K.R <kewin.allenspach@gmail.com>
2022-04-12 11:55:27 +0200

    update packages and collision handling

--- Assets/Scripts/CollisionHandling.cs
+++ Assets/Scripts/CollisionHandling.cs
@@ -1,15 +1,30 @@
 using UnityEngine;
 
+
 public class CollisionHandling : MonoBehaviour
 {
     Rigidbody rb;
 
     void Awake(){
+        
         rb = GetComponent<Rigidbody>();
     }
+    
 
-    
-    
+    void OnCollisionEnter(Collision collision){
+
+        switch (collision.gameObject.tag){
+            case "Start":
+                print("Start");
+                break;
+            case "End":
+                print("End");
+                break;
+            case "Obstacle":
+                print("OI, STOP TOUCHING ME!");
+                break;
+        }
+    }
 
 
 }
