40e72db1ae9a7008c5861e0364e9db9c472e19d6
K.R <kewin.allenspach@gmail.com>
2022-04-22 16:00:08 +0200

    Finish GameMechanics

--- Assets/Scripts/CollisionHandling.cs
+++ Assets/Scripts/CollisionHandling.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class CollisionHandling : MonoBehaviour
@@ -19,12 +20,43 @@ public class CollisionHandling : MonoBehaviour
                 break;
             case "End":
                 print("End");
+                Invoke("StartNextLevelSequence",0.5f);
                 break;
             case "Obstacle":
                 print("OI, STOP TOUCHING ME!");
                 break;
+            default:
+                StartCrashSequence();
+                break;
         }
     }
 
+    void StartCrashSequence(){
+        GetComponent<PlayerMovement>().enabled = false;
+        Invoke("ReloadLevel",1f);
+    }
+    void StartNextLevelSequence(){
+        GetComponent<PlayerMovement>().enabled = false;
+        Invoke("LoadNextLevel",1f);
+    }
 
+    private void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void LoadNextLevel()
+    {
+        if (SceneManager.GetActiveScene().buildIndex + 1 == SceneManager.sceneCountInBuildSettings)
+        {
+            print(SceneManager.GetActiveScene().buildIndex + 1);
+            print(SceneManager.sceneCountInBuildSettings + 1);
+
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
 }
